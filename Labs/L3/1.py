4787490d0c05b50d75f05239e314fa22a8e00d4d
Simiras Teofil <teosimiras@gmail.com>
2024-10-24 16:22:38 +0300

    l3

---
+++ Labs/L3/1.py
@@ -0,0 +1,21 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+N = 8
+F = [[math.e**(-2j * math.pi * m * n / N) for n in range(N)] for m in range(N)]
+F1 = np.array(F)
+x_imaginary = np.imag(F1)
+x_real = np.real(F1)
+
+fig, axs = plt.subplots(N, figsize=(8, 12))
+for i in range(N):
+    axs[i].plot(x_real[i], label='Partea Reală')
+    axs[i].plot(x_imaginary[i], label='Partea Imaginară')
+    axs[i].legend()
+plt.tight_layout()
+plt.show()
+
+F_conj_transpose = np.conj(F1).T
+FFH = np.matmul(F1, F_conj_transpose)
+print(np.allclose(FFH, N * np.eye(N)))
---
+++ Labs/L3/2.py
@@ -0,0 +1,22 @@
+import math 
+import matplotlib.pylab as plt
+import numpy as np
+
+f = 10
+T = 1
+Fs = 10000
+omega = [3, 7, 15, f]
+t = np.linspace(0, T, Fs, endpoint=False)
+s = np.sin(2 * np.pi * f * t)
+
+fig, axs = plt.subplots(2, 2, figsize=(10, 8))
+n = np.arange(len(t))
+
+for idx, o in enumerate(omega):
+    z = [s[i]*math.e ** (-2 * np.pi * 1j * o * i / len(s)) for i in range(len(s))]
+    ax = axs[idx // 2][idx % 2]
+    sc = ax.scatter(np.real(z), np.imag(z), c=np.abs(z), cmap='viridis', s=1)
+    ax.set_aspect('equal')
+    ax.set_title(f'ω = {o}')
+plt.tight_layout()
+plt.show()
