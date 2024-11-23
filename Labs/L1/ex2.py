871495b8ea6d113392292dd332729e014434573c
teofil <teosimiras@gmail.com>
2024-11-24 01:51:52 +0200

    reorganizare

--- Labs/L1/ex2.py
+++ Labs/L1/ex2.py
@@ -1,35 +1,43 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-fig, axs = plt.subplots(6)
-
-#a)
 t1 = np.linspace(0, 2000, 1600)
-x1 = np.sin(2*np.pi*(1/400)*t1)
+x1 = np.sin(2 * np.pi * (1 / 400) * t1)
+plt.figure()
+plt.plot(t1, x1)
+plt.show()
 
-axs[0].plot(t1, x1)
-
-
-#b)
+# b)
 t2 = np.linspace(0, 3, 300)
-x2 = np.sin(2*np.pi*(1/800)*t2)
+x2 = np.sin(2 * np.pi * 800 * t2)
+plt.figure()
+plt.plot(t2, x2)
+plt.show()
 
-axs[1].plot(t2, x2)
+# c)
+t3 = np.linspace(0, 0.3, 1600)
+x3 = 300 * t3 % 10
+plt.figure()
+plt.plot(t3, x3)
+plt.show()
 
-
-#c) 
-t3 = np.linspace(0, 0.3, 1600) 
-x3 = 300*t3%1
-
-axs[2].plot(t3, x3)
-
-#d)
+# d)
 t4 = np.linspace(0, 0.3, 5000)
-x4 = np.sign(np.sin(t4*300))
-axs[3].plot(t4, x4)
+x4 = np.sign(np.sin(t4 * 300))
+plt.figure()
+plt.plot(t4, x4)
+plt.show()
 
-#e)
-t5 = np.linspace(0, 1, 50)
+# e)
+t5 = np.random.rand(128, 128)
+plt.figure()
+plt.imshow(t5, cmap="viridis")
+plt.show()
 
-plt.tight_layout()
+# f)
+t6 = np.eye(128, 128)
+for i in range(len(t6)):
+    t6[i, i] = i
+plt.figure()
+plt.imshow(t6, cmap="plasma")
 plt.show()
--- Labs/L2/p8.py
+++ Labs/L2/p8.py
@@ -1,1 +1,44 @@
 
+#8
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+Fs = 1000
+T = 1
+alpha = np.linspace(-np.pi / 2, np.pi / 2, Fs)
+
+
+x1 = np.sin(alpha)  
+x2 = alpha          
+x3 = alpha / (1 + (alpha ** 2) / 6)  
+
+
+error_approx = np.abs(x2 - x1) 
+error_pade = np.abs(x3 - x1)   
+
+
+fig, axs = plt.subplots(3, figsize=(8, 10))
+fig.suptitle('Aproximări pentru sin(x)')
+
+
+axs[0].plot(alpha, x1, label="sin(x) exact")
+axs[0].plot(alpha, x2, label="sin(x) ≈ x", linestyle="--")
+axs[0].plot(alpha, x3, label="Pade", linestyle=":")
+axs[0].set_title('Funcții')
+axs[0].legend()
+axs[0].grid()
+
+
+axs[1].semilogy(alpha, error_approx)
+axs[1].set_title('Eroare sin(x) ≈ x')
+axs[1].grid()
+
+
+axs[2].semilogy(alpha, error_pade)
+axs[2].set_title('Eroare sin(x) Pade')
+axs[2].grid()
+
+plt.tight_layout()
+plt.show()
+
